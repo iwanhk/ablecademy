54867eb8d55d810d5243336859ab99906ea919d3
iwanhk <iwan@isotop.top>
2023-07-23 16:01:39 +0800

    Initial Version

---
+++ assets/test-901958d7.js
@@ -0,0 +1,1 @@
+import{t as e}from"./tabbar-f059da18.js";import{o as t,c as a,d as r}from"./index-951dfb57.js";const m={__name:"test",setup(o){return(c,s)=>(t(),a("div",null,[r(e)]))}};export{m as default};
